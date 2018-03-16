9465e739ebbb5482612922ad44dd0fd222d928fb
uHappyLogic <kosiak.lukasz@gmail.com>
2018-03-16 19:10:11 +0100

    zad 2

--- Lab1/Lab1/MainWindow.xaml.cs
+++ Lab1/Lab1/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Forms;
+using TreeView = System.Windows.Controls.TreeView;
 
 namespace Lab1
 {
@@ -23,6 +14,49 @@ namespace Lab1
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            var dlg = new FolderBrowserDialog()
+            {
+                Description = "Select directory to open",
+                ShowNewFolderButton = false
+            };
+
+            var result = dlg.ShowDialog();
+
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                DirectoryTreeView.Items.Clear();
+                AddToTree(DirectoryTreeView.Items, dlg.SelectedPath);
+            }
+        }
+
+        private void AddToTree(ItemCollection items, string path)
+        {
+            TreeViewItem currentDirectory = new TreeViewItem
+            {
+                Header = Path.GetFileName(path),
+                Tag = path
+            };
+
+            items.Add(currentDirectory);
+
+            if ((File.GetAttributes(path) & FileAttributes.Directory) != 0)
+            {
+                DirectoryInfo directoryInfo = new DirectoryInfo(path);
+
+                foreach (var subdirectory in directoryInfo.GetDirectories())
+                {
+                    AddToTree(currentDirectory.Items, subdirectory.FullName);
+                }
+
+                foreach (var files in directoryInfo.GetFiles())
+                {
+                    AddToTree(currentDirectory.Items, files.FullName);
+                }
+            }
         }
     }
 }
