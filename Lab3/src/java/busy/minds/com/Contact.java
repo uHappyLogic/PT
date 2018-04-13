dca4605c4f2c26d416a73914cb7b39c81c32bcea
uHappyLogic <kosiak.lukasz@gmail.com>
2018-04-14 01:53:50 +0200

    improvements

--- Lab3/src/java/busy/minds/com/Contact.java
+++ Lab3/src/java/busy/minds/com/Contact.java
@@ -1,7 +1,19 @@
 package busy.minds.com;
 
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.RequestScoped;
 
-public class Contact {
+
+@RequestScoped
+@ManagedBean
+public class Contact implements Serializable {
+    
+    @ManagedProperty(value = "#{contactsManager}")
+    ContactsManager contactsManager;
+    
+    private static final long serialVersionUID = 1L;
     
     String name;
     String surname;
@@ -9,51 +21,67 @@ public class Contact {
     String telephone;
     String group;
         
-     boolean editable;
-     public boolean isEditable(){
-         return editable;
-     }
+    boolean editable;
      
-     public void setEditable(boolean editable) {
-		this.editable = editable;
-	}
+    public Contact() {
         
-        public Contact(String name, String surname, String city, String telephone,
-                String group){
-            this.name = name;
-            this.surname = surname;
-            this.city = city;
-            this.telephone = telephone;
-            this.group = group;
-        }
-        public String getName(){
-            return name;
-        }
-        public void setName(String name){
-            this.name = name;
-        }
-        public String getSurname(){
-            return surname;
-        }
-        public void setSurname(String surname){
-            this.surname = surname;
-        }
-        public String getCity(){
-            return city;
-        }
-        public void setCity(String city){
-            this.city = city;
-        }
-        public String getTelephone(){
-            return telephone;
-        }
-        public void setTelephone(String telephone){
-            this.telephone = telephone;
-        }
-        public String getGroup(){
-            return group;
-        }
-        public void setGroup(String group){
-            this.group = group;
-        }
-    }    
+    } 
+     
+    public Contact(String name, String surname, String city, String telephone, String group){
+        this.name = name;
+        this.surname = surname;
+        this.city = city;
+        this.telephone = telephone;
+        this.group = group;
+    } 
+     
+    public void setEditable(boolean editable) {
+        this.editable = editable;
+    }
+         
+    public String addAction(){
+        System.err.println("Conta is null" + contactsManager == null ? "yes" : "no");
+        contactsManager.getContactList().add(this);
+        return null;        
+    }
+    
+    public boolean isEditable(){
+        return editable;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public String getSurname(){
+        return surname;
+    }
+    public void setSurname(String surname){
+        this.surname = surname;
+    }
+    public String getCity(){
+        return city;
+    }
+    public void setCity(String city){
+        this.city = city;
+    }
+    public String getTelephone(){
+        return telephone;
+    }
+    public void setTelephone(String telephone){
+        this.telephone = telephone;
+    }
+    public String getGroup(){
+        return group;
+    }
+    public void setGroup(String group){
+        this.group = group;
+    }
+    
+    public void setContactsManager(ContactsManager contactsManager) {
+        this.contactsManager = contactsManager;
+    }
+}    
--- Lab3/src/java/busy/minds/com/ContactsManager.java
+++ Lab3/src/java/busy/minds/com/ContactsManager.java
@@ -1,109 +1,87 @@
 package busy.minds.com;
 
-import java.io.Serializable;
-import javax.faces.bean.SessionScoped;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
-import java.util.Collections;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 
-@SessionScoped
-@ManagedBean(name="ContactsManager")
-public class ContactsManager implements Serializable {
+@ManagedBean
+@ApplicationScoped
+public class ContactsManager {
     
-    private static final long serialVersionUID = 1L;
     String name;
     String surname;
     String city;
     String telephone;
     String group;
     
-    private boolean sortAscending = true;
+    private int sortDirection = 1;
     
-    public String sortBySurname() {
+    public void sortBySurname() {
 	    
-	   if(sortAscending){
-		//ascending order
-		Collections.sort(contactList, new Comparator<Contact>() {
-			@Override
-			public int compare(Contact c1, Contact c2) {
-				return c1.getSurname().compareTo(c2.getSurname());
-			}
-		});
-		sortAscending = false;
-	   }else{
-		//descending order
-		Collections.sort(contactList, new Comparator<Contact>() {
-			@Override
-			public int compare(Contact c1, Contact c2) {
-				return c2.getSurname().compareTo(c1.getSurname());		
-			}
-		});
-		sortAscending = true;
-	   }
+        contactList.sort(
+            (Contact c1, Contact c2) -> c1.getSurname().compareTo(c2.getSurname()) * sortDirection
+        );
 
-	   return null;
-	}
+        sortDirection *= -1;
+    }
     
-        public String getName(){
-            return name;
-        }
-        public void setName(String name){
-            this.name = name;
-        }
-        public String getSurname(){
-            return surname;
-        }
-        public void setSurname(String surname){
-            this.surname = surname;
-        }
-        public String getCity(){
-            return city;
-        }
-        public void setCity(String city){
-            this.city = city;
-        }
-        public String getTelephone(){
-            return telephone;
-        }
-        public void setTelephone(String telephone){
-            this.telephone = telephone;
-        }
-        public String getGroup(){
-            return group;
-        }
-        public void setGroup(String group){
-            this.group = group;
-        }
+    @PostConstruct
+    public void fillDb() {
+        contactList = new ArrayList<>(
+            Arrays.asList(
+                new Contact("Tom", "Hardy", "Bromberg", "12533378", "Friends"),
+                new Contact("Christian", "Bale", "Danzig", "232323148", "Others"),
+                new Contact("Heath", "Ledger", "Posen", "35312674", "Family")
+        ));
+    }
+    
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getSurname(){
+        return surname;
+    }
+    public void setSurname(String surname){
+        this.surname = surname;
+    }
+    public String getCity(){
+        return city;
+    }
+    public void setCity(String city){
+        this.city = city;
+    }
+    public String getTelephone(){
+        return telephone;
+    }
+    public void setTelephone(String telephone){
+        this.telephone = telephone;
+    }
+    public String getGroup(){
+        return group;
+    }
+    public void setGroup(String group){
+        this.group = group;
+    }
     
     //name, surname, city, telephone nr, group
-    private static final ArrayList<Contact> contactList = new ArrayList<>(
-        Arrays.asList(
-            new Contact("Tom", "Hardy", "Bromberg", "12533378", "Friends"),
-            new Contact("Christian", "Bale", "Danzig", "232323148", "Others"),
-            new Contact("Heath", "Ledger", "Posen", "35312674", "Family")
-    ));
+    private ArrayList<Contact> contactList;
     
     public ArrayList<Contact> getContactList(){
         return contactList;
     };
-    
-    public String addAction(){
-        Contact contact = new Contact(this.name, this.surname, this.city,
-            this.telephone, this.group);
-                contactList.add(contact);
-                return null;        
-            }
     
     public String deleteAction(Contact contact){
                 contactList.remove(contact);
                 return null;        
             }
     
-    public String editAction(Contact contact){
+    public void editAction(Contact contact){
         contact.setEditable(true);
-        return null;
     }
     
     public String saveAction(){
